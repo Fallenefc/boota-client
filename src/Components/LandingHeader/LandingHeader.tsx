4a735c913e8f96187af9b42e1a179a8304255691
Fallenefc <arylmoraesn@gmail.com>
2021-05-24 15:16:20 -0600

    landing page almost done

--- src/Components/LandingHeader/LandingHeader.tsx
+++ src/Components/LandingHeader/LandingHeader.tsx
@@ -1,38 +1,37 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import logo from '../../Assets/logo2.svg';
 import './styles.css';
 
 export const LandingHeader = () => {
 
-    const history = useHistory();
-
-    const handleClick = (event: any) => {
-      event.target.name === 'login'
-        ? history.push({ pathname: '/login' })
-        : history.push({ pathname: '/signup' });
-    };
-
     return (
         <div className="landing-header">
-          <div className="left-content">
-            <span>About</span>
-            <span>Contact</span>
-          </div>
-          <div className="middle-content">
-            <img src={logo} height="70px" alt="logo" />
-          </div>
-          <div className="right-content">
-            <button className="login-btn" name="login" onClick={handleClick} type="button">
-              Log In
-            </button>
-            <button className="signup-btn" name="signup" onClick={handleClick} type="button">
-              Sign Up
-            </button>
-          </div>
-          <div className="mobile-menu">
-            <i className="fa fa-bars"></i>
-          </div>
+            <div className="left-content">
+                <a href="/about">
+                    <span>About</span>
+                </a>
+                <a href='/contact'>
+                    <span>Contact</span>
+                </a>
+            </div>
+            <div className="middle-content">
+                <img src={logo} height="70px" alt="logo" />
+            </div>
+            <div className="right-content">
+                <a href="/login">
+                    <button className="login-btn" name="login" type="button">
+                        Log In
+                    </button>
+                </a>
+                <a href="/signup">
+                    <button className="signup-btn" name="signup" type="button">
+                        Sign Up
+                    </button>
+                </a>
+            </div>
+            <div className="mobile-menu">
+                <i className="fa fa-bars"></i>
+            </div>
         </div>
     )
 }
