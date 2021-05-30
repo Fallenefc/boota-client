4bf9f1f4caa0059f70436a5a2ed6115bc34fdf51
Fallenefc <arylmoraesn@gmail.com>
2021-05-30 17:34:02 -0600

    added dropdown to app

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { State } from '../../Store/reducer';
@@ -8,6 +8,11 @@ import { addUser } from '../../Store/actions';
 
 export default function Header(): ReactElement<React.FC> {
   const user = useSelector((state: State) => state.user);
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+      setDropdown(!dropdown);
+  }
 
   const dispatch = useDispatch();
   const history = useHistory();
@@ -35,8 +40,24 @@ export default function Header(): ReactElement<React.FC> {
           {user?.name}
           !
         </div>
-        <div className="logout-button">
-          <button onClick={handleLogout} type="button">Logout</button>
+        <div className="mobile-menu">
+          <i className="fa fa-bars" onClick={toggleDropdown}></i>
+          {dropdown ?
+            <div className="dropdown-content">
+              <i className="fa fa-times modal-close" onClick={toggleDropdown}></i>
+              <section className='anchors'>
+                <a href='/'>Dashboard</a>
+                <a href='/viewQuestions'>View Questions</a>
+                <a href='/addQuestion'>Add Questions</a>
+                <a href='/viewQuizzes'>View Quizzes</a>
+                <a href='createQuiz'>Create Quiz</a>
+              </section>
+              <section className="login" onClick={handleLogout}>
+                <a>Logout</a>
+              </section>
+            </div>
+            : null
+          }
         </div>
       </div>
     </header>
