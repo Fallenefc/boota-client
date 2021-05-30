1ada9113569e63ddbb275e0a58b0bd8a010ff760
Fallenefc <arylmoraesn@gmail.com>
2021-05-30 17:27:45 -0600

    added dropdown

--- src/Components/LandingHeader/LandingHeader.tsx
+++ src/Components/LandingHeader/LandingHeader.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../Assets/logo2.svg';
 import './styles.css';
 
 export const LandingHeader = () => {
+
+    const [dropdown, setDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdown(!dropdown);
+    }
 
     return (
         <div className="landing-header">
@@ -31,7 +37,22 @@ export const LandingHeader = () => {
                 </a>
             </div>
             <div className="mobile-menu">
-                <i className="fa fa-bars"></i>
+                <i className="fa fa-bars" onClick={toggleDropdown}></i>
+                { dropdown ?
+                <div className="dropdown-content">
+                    <i className="fa fa-times modal-close" onClick={toggleDropdown}></i>
+                    <section className='anchors'>
+                        <a>Demo</a>
+                        <a>Pricing</a>
+                        <a>About Us</a>
+                        <a>Contact</a>
+                    </section>
+                    <section className="login">
+                        <a>Login</a>
+                    </section>
+                </div>
+                : null
+                }
             </div>
         </div>
     )
